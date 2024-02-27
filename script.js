5eb66152fb97cfc25a78194f9378c13cb98634b4
Sam <sam123dbz@gmail.com>
2024-02-27 18:32:01 +0500

    Added SFX functionality

--- script.js
+++ script.js
@@ -27,29 +27,31 @@ const endgameModal = document.querySelector("#endgameModal");
 const endgameMsg = document.querySelector("#endgameMsg");
 
 
+function stopRoundSfx() {
+    roundSfx.currentTime = 0;
+    roundSfx.pause();
+};
+
+
 function checkPlayerScore(playerScore) {
     if (playerScore == 5) {
         endgameModal.style.display = "block";
         endgameMsg.textContent = "You win";
-
-        const victorySfx = document.createElement("audio");
+        stopRoundSfx();
+        victorySfx = document.querySelector("#victorySfx");
         victorySfx.src = getRandomSfx("victory");
         victorySfx.autoplay = true;
-
-        endgameModal.appendChild(victorySfx);
     }
 };
-
 
 function checkEnemyScore(enemyScore) {
     if (enemyScore == 5) {
         endgameModal.style.display = "block";
         endgameMsg.textContent = "You died";
-        const defeatSfx = document.createElement("audio");
+        stopRoundSfx();
+        defeatSfx = document.querySelector("#defeatSfx");
         defeatSfx.src = getRandomSfx("defeat");
         defeatSfx.autoplay = true;
-
-        endgameModal.appendChild(defeatSfx);
     }
 };
 
@@ -59,12 +61,15 @@ let playerScore = 0;
 let enemyScore = 0;
 let scoreInfo = document.querySelector(".score-info");
 let scoreMessage = document.querySelector(".score-message");
+let roundSfx = document.querySelector("#roundSfx");
+roundSfx.autoplay = true;
 
 // Takes two inputs, from the Player, and the random one
 function playGame (playerSelection, enemySelection) {
     if (playerSelection === enemySelection) {
         scoreInfo.textContent = "It's a tie!";
         scoreMessage.textContent = `${playerSelection} ties with ${enemySelection}`;
+        roundSfx.setAttribute("src", "sfx/round-tie-sfx.mp3");
     } else if (enemySelection === "LANCE" && playerSelection === "SWORD" ||
     enemySelection === "SWORD" && playerSelection === "AXE" ||
     enemySelection === "AXE" && playerSelection === "LANCE") {
@@ -73,12 +78,14 @@ function playGame (playerSelection, enemySelection) {
         enemyScore++;
         enemyScoreText.textContent = `Enemy: ${enemyScore}`;
         checkEnemyScore(enemyScore);
+        setRoundDefeatSfx();
     } else {
         scoreInfo.textContent = "You win the round!";
         scoreMessage.textContent = `${playerSelection} beats ${enemySelection}`;
         playerScore++;
         playerScoreText.textContent = `Player: ${playerScore}`;
         checkPlayerScore(playerScore);
+        setRoundVictorySfx();
     }
 
     switch(enemySelection) {
@@ -98,6 +105,18 @@ const buttons = document.querySelector(".buttons");
 
 const playerWeapon = document.querySelector("#playerWeapon");
 const enemyWeapon = document.querySelector("#enemyWeapon");
+
+function setRoundDefeatSfx() {
+    if (enemyScore < 5) {
+        roundSfx.setAttribute("src", "sfx/round-defeat-sfx.mp3");
+    };
+}
+
+function setRoundVictorySfx() {
+    if (playerScore < 5) {
+        roundSfx.setAttribute("src", "sfx/round-win-sfx.mp3");
+    };
+}
 
 function setEnemyWeaponImage(imageName) {
     enemyWeapon.setAttribute("src", `images/${imageName}.png`);
@@ -167,6 +186,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const originalScoreInfo = document.querySelector(".score-info").textContent;
 const originalScoreMessage = document.querySelector(".score-message").textContent;
+const audioFiles = document.querySelectorAll("audio");
+
+audioFiles.forEach((audio) => {
+    audio.volume = 0.5; 
+ });
+
+function stopMainSfx() {
+    victorySfx.currentTime = defeatSfx.currentTime = 0;
+    victorySfx.pause();
+    defeatSfx.pause();
+};
+
 
 const endBtn = document.querySelector("#restartBtn");
 endBtn.addEventListener("click", () => {
@@ -177,6 +208,7 @@ endBtn.addEventListener("click", () => {
     document.querySelector(".score-info").textContent = originalScoreInfo;
     document.querySelector(".score-message").textContent = originalScoreMessage;
     endgameModal.style.display = "none";
+    stopMainSfx();
 });
 
 /* Add bg; Add music; Add colors and better layout; Add sfx; */
